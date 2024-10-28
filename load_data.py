4de3ee704f2ee30c410a6ce6940c69e9c316d6f7
Ghritachi Mahajani <ghritachi.mahajani@gmail.com>
2024-10-28 13:43:47 -0600

    Ghritachi - initial setup

---
+++ load_data.py
@@ -0,0 +1,11 @@
+from roboflow import Roboflow
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+rf = Roboflow(api_key=os.getenv('herial_key'))
+
+project = rf.workspace("new-workspace-qsuag").project("heridal-t428z")
+version = project.version(2)
+dataset = version.download("yolov8")
